1cbda7c6a8f2bd9367d3a56588583dc0379a15e9
MDM Seva User <user@mdmseva.local>
2025-12-03 09:15:48 +0530

    Build frontend assets for deployment

--- public/build/assets/AdminLayout-B_iUlsBd.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-n9EZSqaC.js";import{A as a}from"./AuthenticatedLayout-BTAgNKUi.js";function n({children:t,...o}){return r.jsx(a,{...o,children:t})}export{n as A};
---
+++ public/build/assets/AdminLayout-mXuK6Fak.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-CMKKpqEa.js";import{A as a}from"./AuthenticatedLayout-DXtD_i-H.js";function n({children:t,...o}){return r.jsx(a,{...o,children:t})}export{n as A};
